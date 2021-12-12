dcf51225dd8bf9e043f50dbb67b46be0cd0f1249
kirannambiar12 <kiran.un@1stmain.co>
2021-12-12 18:37:44 +0530

    added some assets and package installation

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,26 @@
+import { Route, Routes } from "react-router-dom";
+import GlobalStyle from "./assets/styles/globalStyles";
+import Homepage from "./components/home";
 
 function App() {
   return (
     <div className="App">
-
+      <GlobalStyle />
+      <Routes>
+        <Route path='/' element={<Homepage />} />
+        {/* <Route path="/about" component={About} />
+        <Route path="/work" component={Technologies} />
+        <Route path="/contact" component={Contact} />
+        <Route path="/widgets/weather-app" component={TemperatureTracker} />
+        <Route path="/widgets/mars-rover-images" component={Nasa} />
+        <Route path="/widgets/news" component={NewsApi} />
+        <Route path="/widgets/jokes" component={Jokes} />
+        <Route path="*" component={NotFound} />
+        <Route element={<PrivateRoute />}>
+          <Route path='/' element={<Home />} />
+          <Route path='/my-account' element={<Account />} />
+        </Route> */}
+      </Routes>
     </div>
   );
 }
